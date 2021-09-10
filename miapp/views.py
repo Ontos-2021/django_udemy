f7a9173d886d97b39b4cfb1cc81eee6494bc9217
_Ontos_ <josemercado.musica@gmail.com>
2021-09-10 03:02:43 -0300

    Navegación entre rutas

--- miapp/views.py
+++ miapp/views.py
@@ -5,6 +5,24 @@ from django.shortcuts import render, HttpResponse
 # MVC = Modelo Vista Controlador (El controlador hace acciones)
 # MVT = Modelo Templates Vistas -> La vista hace acciones (Métodos)
 
+layout = """
+    
+    <h1>Sitio web con Django | Ontos</h1>
+    <hr/>
+    <li>
+        <a href="/inicio">Inicio</a>
+    </li>
+    <li>
+        <a href="/hola-mundo">Hola Mundo</a>
+    </li>
+    <li>
+        <a href="/pagina-pruebas">Página de pruebas</a>
+    </li>
+</ul>
+<hr/>
+"""
+
+
 def index(request):
 
     html = """
@@ -18,18 +36,18 @@ def index(request):
         numero += 1
 
     html += "</ul>"
-    return HttpResponse(html)
+    return HttpResponse(layout + html)
 
 
 def hola_mundo(request):
-    return HttpResponse("""
+    return HttpResponse(layout + """
     <h1>¡Hola Mundo con Django!</h1>
     <p>Esta es mi primera vista con Django</p>
     """)
 
 
 def pagina(request):
-    return HttpResponse("""
+    return HttpResponse(layout + """
     <h1>Página de mi web</h1>
     <p>Creado por Ontos</p>
     """)
