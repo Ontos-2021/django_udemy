af3c94257fbbc160f66ae10f25a0390fc4ac9bc1
_Ontos_ <josemercado.musica@gmail.com>
2021-10-09 14:58:30 -0300

    Crear filtros personalizados

---
+++ miapp/templatetags/filters.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name="saludo")
+
+def saludo(value):
+    largo = ""
+    if len(value) >= 8:
+        largo = "Tu nombre es muy largo"
+
+    
+    return f"<h1 style='background:green;color:white;'>Bienvenido, {value}</h1>" + largo
+
