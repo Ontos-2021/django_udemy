941440d9d13f019f0dca18a680ecc81dbfb484fc
_Ontos_ <josemercado.musica@gmail.com>
2021-10-09 15:26:40 -0300

    Crear modelos

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    public = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    created_at = models.DateField()
