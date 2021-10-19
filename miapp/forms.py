b4c490cec3dcb9ff0569530e72b7c6bdfa95db85
_Ontos_ <josemercado.musica@gmail.com>
2021-10-19 00:30:28 -0300

    Personalizar formularios

--- miapp/forms.py
+++ miapp/forms.py
@@ -5,13 +5,33 @@ class FormArticle(forms.Form):
 
 
     title = forms.CharField(
-         label = "Título"
+         label = "Título",
+         max_length=40,
+         # required=False,
+         widget=forms.TextInput(
+             attrs={
+                 "placeholder": "Ingresá el título",
+                 "class": "titulo_form_article"
+             }
+         )
     )
      
     content = forms.CharField(
          label = "Contenido",
-         widget=forms.Textarea
+         widget=forms.Textarea(
+             attrs={
+                 "placeholder": "Ingresá el contenido",
+                 "class": "contenido_form_article"
+             }
+         )
     )
+
+    # Esta es otra forma de configurar el Widget
+    content.widget.attrs.update({
+        "placeholder": "Ingresá el contenido",
+        "class": "contenido_form_article",
+        "id": "contenido_form"
+    })
 
     public_options = [
         (1, "Sí"),
